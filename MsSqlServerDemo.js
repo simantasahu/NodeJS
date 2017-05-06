fa8d30ad8143eb7ccfdf16928e947a7eee06c8c1
simantasahu <Simanta@AdminPC.cybrain.local>
2017-05-06 18:39:55 +0530

    MSSqlServer Connection Testing

---
+++ MsSqlServerDemo.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'admin@123',
+        server: 'ADMINISTRATORPC\\SQLSERVER2016', 
+        database: 'dbAPSNoidaWeb'
+    };
+    
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select top 10 * from tblClassMaster', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            res.end();
+            sql.close();            
+        });
+    });    
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
